27cb32023fc0524e1a28a66a37af80b20e75a3c4
Nikolai Weibull <now@bitwi.se>
2013-09-05 15:12:36 +0200

    Update dependencies

--- Rakefile
+++ Rakefile
@@ -4,11 +4,7 @@ require 'inventory-rake-1.0'
 
 load File.expand_path('../lib/yard-value-1.0/version.rb', __FILE__)
 
-Inventory::Rake::Tasks.define YARDValue::Version, :gem => proc{ |_, s|
-  s.author = 'Nikolai Weibull'
-  s.email = 'now@bitwi.se'
-  s.homepage = 'https://github.com/now/yard-value'
-}
+Inventory::Rake::Tasks.define YARDValue::Version
 
 Inventory::Rake::Tasks.unless_installing_dependencies do
   require 'lookout-rake-3.0'
--- lib/yard-value-1.0/version.rb
+++ lib/yard-value-1.0/version.rb
@@ -3,13 +3,25 @@
 require 'inventory-1.0'
 
 module YARDValue
-  Version = Inventory.new(1, 2, 3){
+  Version = Inventory.new(1, 2, 4){
+    authors{
+      author 'Nikolai Weibull', 'now@disu.se'
+    }
+
+    homepage 'http://disu.se/software/yard-value/'
+
+    licenses{
+      license 'LGPLv3+',
+              'GNU Lesser General Public License, version 3 or later',
+              'http://www.gnu.org/licenses/'
+    }
+
     def dependencies
       super + Inventory::Dependencies.new{
-        development 'inventory-rake', 1, 4, 0
+        development 'inventory-rake', 1, 6, 0
         development 'lookout', 3, 0, 0
-        development 'lookout-rake', 3, 0, 0
-        runtime 'yard', 0, 8, 0, :feature => 'yard'
+        development 'lookout-rake', 3, 1, 0
+        runtime 'yard', 0, 8, 7, :feature => 'yard'
       }
     end
   }
